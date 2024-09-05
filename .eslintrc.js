9b731841c0fd3e615853945f332998963943ed4c
thereareloveyou <thereareloveyou32@gmail.com>
2024-09-05 23:05:18 +0300

    Create auth user model and setup settings MongoDB

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  extends: ['plugin:prettier/recommended', 'prettier'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': 0,
+    'no-console': 1
+  },
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModel } from 'src/user/user.model'
+import { ConfigModule } from '@nestjs/config'
+
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([{ typegooseClass: UserModel, schemaOptions: { collection: 'User' } }]),
+    ConfigModule,
+  ],
+})
+export class AuthModule {}
